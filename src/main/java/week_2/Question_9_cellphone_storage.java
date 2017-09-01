f961388a24371c836ca46bf44cec2cbae42a93dd
clara <clara@clara.com>
2017-09-01 08:45:06 -0500

    Cellphone updates, fixed tests

--- src/main/java/week_2/Question_9_cellphone_storage.java
+++ src/main/java/week_2/Question_9_cellphone_storage.java
@@ -6,29 +6,33 @@ package week_2;
 
  •	App A syncs and downloads data every hour, and downloads 0.5KB each time
  •	App B syncs and downloads daily, and downloads 2KB every time
- •	App C, when installed, is 1MB in size. This app syncs and downloads every 4 hours, and every time it syncs it downloads 1% of its current size. The new data it downloads count towards the app's size, so you'll need to keep track of the current size of the app.
+ •	App C, when installed, is 1MB in size  (assume this is equal to 1000KB). This app syncs and
+       downloads every 4 hours, and every time it syncs it downloads 1% of its current size.
+       The new data it downloads count towards the app's size, so you'll need to keep track of
+       the current size of the app.
 
- With all apps installed, the phone has 5MB of free space.
+ With all apps installed, the phone has 5MB (or 5000KB) of free space.
  When the phone starts, each app syncs and downloads, and then repeats to their own schedule.
 
- With all 3 apps running continuously, how long will it be before the phone runs out of space?
+ With all 3 apps running continuously, how long, in hours, will it be before the phone runs out of space?
 
  Assume the phone is running continuously and nothing else is using space on the phone.
  Assume that 1KB = 1000 bytes and 1MB = 1000,000 bytes.
 
- Hint 1: the modulus operator is helpful. An expression like  ( number % 4 == 0 ) is true if number divides evenly by 4.
- Hint 2: the answer is several thousand hours.
+ Hint 1: the modulo operator is helpful. An expression like  ( number % 4 == 0 ) is true if number divides evenly by 4.
+ Hint 2: the answer is several hundred hours.
  Hint 3: This problem is a little trickier than the other ones, but it can be done :)
 
  */
 public class Question_9_cellphone_storage {
 
     public static void main(String[] args) {
-        double hoursToFill = new Question_9_cellphone_storage().calculateTimeToFillPhone(5000, 0.5, 2, 1, 1000);
+        
+        int hoursToFill = new Question_9_cellphone_storage().calculateTimeToFillPhone(5000, 0.5, 2, 1, 1000);
         System.out.println(hoursToFill);
     }
 
-    public double calculateTimeToFillPhone(int freeSpaceKB, double appAHourlyDownloadKB, double appBDailyDownloadKB,
+    public int calculateTimeToFillPhone(double freeSpaceKB, double appAHourlyDownloadKB, double appBDailyDownloadKB,
                                            double appCPercentDownload, double appCSizeKB) {
 
         // TODO Calculate and return the number of hours until the phone runs out of space
@@ -36,11 +40,11 @@ public class Question_9_cellphone_storage {
         // Use the method arguments.
 
         int hours = 0;
-
-
-
+    
+        // TODO write your code here
+        
         return hours;  // TODO replace with your own calculated value
 
     }
-
+    
 }
--- src/test/java/week_2/Question_9_cellphone_storageTest.java
+++ src/test/java/week_2/Question_9_cellphone_storageTest.java
@@ -7,8 +7,14 @@ public class Question_9_cellphone_storageTest extends TestCase {
     public void testCalculateTimeToFillPhone() throws Exception {
 
         Question_9_cellphone_storage q10 = new Question_9_cellphone_storage();
-        assertEquals(4714, q10.calculateTimeToFillPhone(5000, 0.5, 2, 0.1, 1000));
-        assertEquals(225, q10.calculateTimeToFillPhone(5000, 3, 2.5, 3, 1000));
+        // 696, plus or minus a handful of hours. You might have the right logic but count the hours slightly differently.
+        assertEquals("The answer should be about 696. If you are sure your logic is right, \nplease tell Clara that this test is failing.. ", 696, q10.calculateTimeToFillPhone(5000, 0.5, 2, 1, 1000), 30);
+        
+        // Another example: what if AppA downloads 3KB, AppB downloads 2.5KB, App 3 downloads 3% and starts at 4MB?
+        assertEquals("Ensure you use the calculateTimeToFillPhone arguments. If you are sure your logic is right, \nplease tell Clara that this test is failing.", 108, q10.calculateTimeToFillPhone(5000, 3, 2.5, 3, 4000), 5);
+
+        // Another example: what if AppA downloads 3KB, AppB downloads 2.5KB, App 3 downloads 3% and starts at 4MB?
+        assertEquals("Ensure you use the calculateTimeToFillPhone arguments. If you are sure your logic is right, \nplease tell Clara that this test is failing.", 4718, q10.calculateTimeToFillPhone(5000, 0.5, 2, 0.1, 1000), 100);
 
     }
 }
