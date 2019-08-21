efa9230333f47de0aa69e3ad946742e1e5cff753
clara <clara@minneapolis.edu>
2019-08-21 18:47:20 -0500

    Q3

--- src/main/java/week_2/Question_3_While_Input_Validation.java
+++ src/main/java/week_2/Question_3_While_Input_Validation.java
@@ -25,17 +25,18 @@ public class Question_3_While_Input_Validation {
         /*
         * Complete this method.
         *
-        * Use the intInput method to ask the user for a class code.
+        * Use the intInput method to print a message asking the user for a class code. (don't use positiveIntInput.)
         *
         * A valid class code is a number between 1000 and 2999.
         *
         * In case the user makes a mistake and enters a number lower than 1000 or more than 2999,
-        * write a validation while loop. This loop should
+        * write a validation while loop. This loop should repeat while the data entered is invalid.
+        *
         * */
         
         // TODO delete this line and replace this with your code.
         return 0;
-     
+        
     }
 
 }
--- src/test/java/week_2/Question_3_While_Input_Validation_Test.java
+++ src/test/java/week_2/Question_3_While_Input_Validation_Test.java
@@ -11,6 +11,7 @@ import static org.easymock.EasyMock.anyString;
 import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.assertTrue;
 import static org.powermock.api.easymock.PowerMock.*;
+import static week_2.Question_3_While_Input_Validation.getCollegeClassCode;
 
 
 @RunWith(PowerMockRunner.class)
@@ -19,20 +20,59 @@ import static org.powermock.api.easymock.PowerMock.*;
 public class Question_3_While_Input_Validation_Test {
     
     @Test(timeout=3000)
-    public void testGetCharacterInvalid() {
+    public void testGetCodeRejectNumbersTooHigh() {
         
         mockStatic(InputUtils.class);
-        expect(InputUtils.stringInput(anyString()))
-                .andReturn("")        // Enter something that's too short
-                .andReturn("123456")  // Enter something that's too long
-                .andReturn("ab")  // Enter something that's too long
-                .andReturn("Q");      // Exactly 1 character
+        expect(InputUtils.intInput(anyString()))
+                .andReturn(3010)
+                .andReturn(123456)
+                .andReturn(3000)
+                .andReturn(9999)
+                .andReturn(2454);  // Valid
         replay(InputUtils.class);
         
-        // TODO do thing
+        int code = getCollegeClassCode();
+        assertEquals("The getCollegeClassCode should reject input numbers greater than 2999", 2454, code);
         
     }
     
+    @Test(timeout=3000)
+    public void testGetCodeRejectNumbersTooLow() {
+        
+        mockStatic(InputUtils.class);
+        expect(InputUtils.intInput(anyString()))
+                .andReturn(0)
+                .andReturn(-1123456)
+                .andReturn(999)
+                .andReturn(43)
+                .andReturn(2417);  // Valid
+        replay(InputUtils.class);
+        
+        int code = getCollegeClassCode();
+        assertEquals("The getCollegeClassCode method should reject input numbers smaller than 1000", 2417, code);
+        
+    }
+    
+    @Test(timeout=3000)
+    public void testGetCodeAcceptValidNumbers() {
+        
+        mockStatic(InputUtils.class);
+        expect(InputUtils.intInput(anyString()))
+                .andReturn(2417)
+                .andReturn(2950)
+                .andReturn(1310);
+        replay(InputUtils.class);
+        
+        int code1 = getCollegeClassCode();
+        assertEquals("The getCollegeClassCode method should accept input numbers 2417 first time", 2417, code1);
+    
+        int code2 = getCollegeClassCode();
+        assertEquals("The getCollegeClassCode method should accept input numbers 2950 first time", 2950, code2);
+    
+        int code3 = getCollegeClassCode();
+        assertEquals("The getCollegeClassCode method should accept input numbers 1310 first time", 1310, code3);
+    
+    }
     
     
 }
